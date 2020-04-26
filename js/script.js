7794c88f7b5f4d72f6b19405b5da32c27e9a3638
ImadEddine Lakhdar <imadeddinelakhdar08@gmail.com>
2020-04-26 15:31:47 +0000

    update 2

--- js/script.js
+++ js/script.js
@@ -12,21 +12,21 @@ function onload() {
         document.getElementById("moon").style.display = "none";
     }
     else {
-        console.log("Error")
+        localStorage.setItem("color","#ffff")
+        document.getElementById("sun").style.display = "none";
+        document.getElementById("moon").style.display = "block";
     }
 }
 function change() {
     if(localStorage.color === "#ffff") {
         document.body.style.backgroundColor = "#292C35";
         localStorage.color = "#292C35"
-        theme = "dark";
         document.getElementById("moon").style.display = "none";
         document.getElementById("sun").style.display = "block";
     }
     else if(localStorage.color === "#292C35"){
         document.body.style.backgroundColor = "#ffff";
         localStorage.color = "#ffff"
-        theme = "light";
         document.getElementById("sun").style.display = "none";
         document.getElementById("moon").style.display = "block";
     }
